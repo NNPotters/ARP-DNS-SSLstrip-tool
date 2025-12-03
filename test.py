c4f9bd8fac76d6b3a7cc46b81fb65e2eff999c9b
NNPotters <n.n.potters@student.tue.nl>
2025-12-03 10:38:37 -0800

    Removed test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import scapy.all as scapy
-
-scapy.lsc() # list of scapy commands
-
-#scapy.arping() #Send ARP who-has requests to determine which hosts are up, needs 'net' argument
-#scapy.arpcachepoison() # poison target's ARP cache
-#scapy.arp_mitm() # poison 2 target's ARP cache
